f23c285f6b9cf30f5eb90bd456ac5e032542fc05
carlis10 <carlos.raveglia@gmail.com>
2025-12-04 13:29:13 +0100

    main

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: carlos <carlos@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 12:06:35 by carlos            #+#    #+#             */
-/*   Updated: 2025/07/04 13:04:45 by carlos           ###   ########.fr       */
+/*   Updated: 2025/07/14 13:42:29 by carlos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 int main(void)
 {
-	ScavTrap claptrap("clap-trap");
-	claptrap.attack("Jack");
-	claptrap.takeDamage(8);
-	claptrap.beRepaired(7);
-	claptrap.takeDamage(8);
-	claptrap.beRepaired(5);
-	claptrap.takeDamage(4);
-	claptrap.guardGate();
+	ScavTrap *claptrap = new ScavTrap("Claptrap");
+	claptrap->attack("Jack");
+	claptrap->takeDamage(8);
+	claptrap->beRepaired(7);
+	claptrap->takeDamage(8);
+	claptrap->beRepaired(5);
+	claptrap->takeDamage(4);
+	claptrap->guardGate();
 	return 0;
 }
